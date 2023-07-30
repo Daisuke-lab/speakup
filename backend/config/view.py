05b1583d58e08ed5c8d0bed6d392b4a37118a176
daisuke <daisuke.zr@icloud.com>
2023-07-30 21:29:37 +0900

    ping undoed

--- backend/config/view.py
+++ backend/config/view.py
@@ -2,12 +2,7 @@
 from django.http import HttpResponse, JsonResponse
 from django.template import RequestContext
 
-def ping(request, id):
+def ping(request):
     #health check用
-    print(request.resolver_match.kwargs)
-    # for key in request.__dict__.keys():
-    #     print("/////////////////////////", key)
-    #     print(request.__dict__[key])
-    # print(request.__dict__["META"].keys())
     return HttpResponse("Hello World!!")
 
